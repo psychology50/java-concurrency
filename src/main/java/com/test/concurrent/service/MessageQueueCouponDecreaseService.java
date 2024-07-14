d9ec64cb259fb6e7fe263df1e0b4de2c37c44195
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-14 22:01:26 +0900

    feat: blocking queue 인터페이스로 busy waiting 상태 개선

--- src/main/java/com/test/concurrent/service/MessageQueueCouponDecreaseService.java
+++ src/main/java/com/test/concurrent/service/MessageQueueCouponDecreaseService.java
@@ -3,16 +3,14 @@ package com.test.concurrent.service;
 import com.test.concurrent.domain.Coupon;
 import com.test.concurrent.repository.CouponRepository;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Queue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
@@ -20,9 +18,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RequiredArgsConstructor
 public class MessageQueueCouponDecreaseService {
     private final CouponRepository couponRepository;
-    private final Queue<String> couponQueue = new ConcurrentLinkedQueue<>();
+    private final BlockingQueue<String> couponQueue = new LinkedBlockingQueue<>();
     private final ExecutorService executorService = Executors.newSingleThreadExecutor(); // 워커 스레드
-    private final AtomicInteger messageCount = new AtomicInteger(0);
+    private final AtomicInteger messageCount = new AtomicInteger(300);
 
     @PostConstruct
     public void init() {
@@ -31,29 +29,26 @@ public class MessageQueueCouponDecreaseService {
 
     public void decreaseStockWithMessagingQueue(Long couponId) {
         couponQueue.offer(couponId + ":" + Thread.currentThread().getId());
-        messageCount.incrementAndGet();
     }
 
     private void processQueue() {
         while (!Thread.currentThread().isInterrupted()) {
-            String message = couponQueue.poll();
+            try {
+                String message = couponQueue.take();
+                log.info("메시지를 처리합니다. message={}", message);
 
-            Long couponId = null;
-            if (message != null)
-                couponId = Long.parseLong(message.split(":")[0]);
+                Long couponId = Long.parseLong(message.split(":")[0]);
 
-            if (couponId != null) {
-                try {
-                    Coupon coupon = couponRepository.findById(couponId)
-                            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 쿠폰입니다."));
-                    coupon.decreaseStock();
-                    couponRepository.save(coupon);
-                    messageCount.decrementAndGet();
-                    log.info("쿠폰 차감을 완료했습니다. couponId={}", couponId);
-                } catch (Exception e) {
-                    log.error("쿠폰 차감 중 에러 발생", e);
-                    throw e;
-                }
+                Coupon coupon = couponRepository.findById(couponId)
+                        .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 쿠폰입니다."));
+                coupon.decreaseStock();
+                couponRepository.save(coupon);
+
+                messageCount.decrementAndGet();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (Exception e) {
+                log.error("메시지 처리 중 에러 발생", e);
             }
         }
     }
@@ -62,5 +57,10 @@ public class MessageQueueCouponDecreaseService {
         while (messageCount.get() > 0) {
             Thread.sleep(100L);
         }
+    }
+
+    @PreDestroy
+    public void close() {
+        executorService.shutdownNow();
     }
 }
