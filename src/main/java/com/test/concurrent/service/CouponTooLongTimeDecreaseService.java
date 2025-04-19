8ec29034f4c9d47f05490237377694c39683c0a0
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-04-19 19:04:03 +0900

    feat: coupon too long time decrease service

---
+++ src/main/java/com/test/concurrent/service/CouponTooLongTimeDecreaseService.java
@@ -0,0 +1,33 @@
+package com.test.concurrent.service;
+
+import com.test.concurrent.aop.DistributedLock;
+import com.test.concurrent.domain.Coupon;
+import com.test.concurrent.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CouponTooLongTimeDecreaseService {
+    private final CouponRepository couponRepository;
+
+    @DistributedLock(key = "'like_' + #couponId", waitTime = 5L, leaseTime = 5L, needNewTransaction = true)
+    public void run(Long couponId) {
+        Coupon coupon = couponRepository.findById(couponId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 쿠폰입니다."));
+
+        // 10초 대기 (락 만료 시간인 5초보다 오래 걸림)
+        try {
+            log.info("Sleeping for 10 seconds...");
+            Thread.sleep(10000);
+            log.info("Woke up after sleep");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Thread interrupted", e);
+        }
+
+        coupon.decreaseStock();
+    }
+}
